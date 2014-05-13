d306d45677c638ed147bc5a44a5392ed075f63ab
Tauran <tauranwood@gmail.com>
2014-05-13 10:46:21 -0600

    Moved most menu importation to seperate file

---
+++ b829/aberhart/universal/menu-importer.js
@@ -0,0 +1,7 @@
+"use strict"
+$( document ).ready(function() {
+	$("#topmenu-import").load( "/b829/aberhart/universal/topmenu.html" );
+	$("#cbe-header").load( "/b829/aberhart/universal/cbe-header.html");
+	$("#rightside").load( "/b829/aberhart/universal/rightside.html" );
+	$("#footers").load( "/b829/aberhart/universal/footers.html" );
+)};
