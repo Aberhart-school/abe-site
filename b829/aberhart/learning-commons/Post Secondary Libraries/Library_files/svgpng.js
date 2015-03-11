96a6d71efadbfc532f121969fade581cf0a94c39
owmygreenmatter <aneroidluxation@gmail.com>
2015-03-10 20:40:04 -0600

    integrating learning commons pages

--- b829/aberhart/learning-commons/Post Secondary Libraries/Library_files/svgpng.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-  if (!Modernizr.svg) {
-    $('img[src*="svg"]').attr('src', function() {
-        return $(this).attr('src').replace('.svg', '.png');
-    });
-  }
-});
-
