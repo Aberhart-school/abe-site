3aaadf94b6039340c45e6e677cd316a56d88c30b
thirstyice <tauranwood@gmail.com>
2015-09-06 14:35:04 -0600

    Fixed indenting and improved readability in all documents

--- document-index-builder.sh
+++ document-index-builder.sh
@@ -8,7 +8,7 @@ HTML="<!DOCTYPE html> <!-- This has to come before any comments (or anything els
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-    
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
